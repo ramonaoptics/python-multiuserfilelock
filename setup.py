c3c5badd7e6794ed0ca9fd87c1de8e461cabf87c
Mark Harfouche <mark.harfouche@gmail.com>
2021-11-23 10:11:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import versioneer
 setup(
     name='multiuserfilelock',
     version=versioneer.get_version(),
-    url='https://github.com/ramonaoptics/multiuserfilelock',
+    url='https://github.com/ramonaoptics/python-multiuserfilelock',
     author='Ramona Optics, Inc.',
     author_email='info@ramonaoptics.com',
     description='A lock to share resources between users based on filelock.',
